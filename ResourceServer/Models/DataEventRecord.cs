97596f269f63bb89e7991f55836bae3a431f80fd
hwang <hwang@BC1HWANG>
2017-01-31 14:57:30 -0500

    Initial database set up

--- ResourceServer/Models/DataEventRecord.cs
+++ ResourceServer/Models/DataEventRecord.cs
@@ -7,5 +7,11 @@ namespace ResourceServer.Models
 {
     public class DataEventRecord
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
