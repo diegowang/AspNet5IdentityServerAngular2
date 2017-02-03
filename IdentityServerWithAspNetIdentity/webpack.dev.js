1b6ed382ad8e5e7339f94b54b7eea5b8d5e52c6d
hwang <hwang@BC1HWANG>
2017-02-03 09:04:35 -0500

    Removed a module loader from webpack.dev

--- IdentityServerWithAspNetIdentity/webpack.dev.js
+++ IdentityServerWithAspNetIdentity/webpack.dev.js
@@ -43,7 +43,6 @@ module.exports = {
                 loaders: [
                     'awesome-typescript-loader',
                     'angular2-template-loader',
-                    'angular-router-loader',
                     'source-map-loader',
                     'tslint-loader'
                 ]
