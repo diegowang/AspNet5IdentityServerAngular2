d2b647f8062a65702b5962c1e0e197d41947372b
hwang <hwang@BC1HWANG>
2017-02-03 09:21:33 -0500

    Minor fix on namespace.

--- IdentityServerWithAspNetIdentity/Filters/SecurityHeadersAttribute.cs
+++ IdentityServerWithAspNetIdentity/Filters/SecurityHeadersAttribute.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace IdentityServerWithAspNetIdentity.Controllers
+namespace IdentityServerWithAspNetIdentity.Filters
 {
     public class SecurityHeadersAttribute : ActionFilterAttribute
     {
