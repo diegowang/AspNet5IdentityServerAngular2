91cb734a1bd953fcffe70e04e0be2a0dc42ed5cf
hwang <hwang@BC1HWANG>
2017-02-06 12:08:29 -0500

    Added an interface for Ticket Validation and related packages.

---
+++ AspNet.Security.CAS/ICasTicketValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+
+namespace AspNet.Security.CAS
+{
+    interface ICasTicketValidator
+    {
+        Task<AuthenticateResult> ValidateTicket(HttpContext context, HttpClient httpClient,
+            AuthenticationProperties properties, string ticket, string service);
+    }
+}
