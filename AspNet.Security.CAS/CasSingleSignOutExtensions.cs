514e6b950cd19ac0fcc885aa0bfccfb1a5ec6454
hwang <hwang@BC1HWANG>
2017-02-08 14:43:28 -0500

    Extension method to add single sign out to application

---
+++ AspNet.Security.CAS/CasSingleSignOutExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+
+namespace AspNet.Security.CAS
+{
+    public static class CasSingleSignOutExtensions
+    {
+        public static IApplicationBuilder UseCasSingleSignOut(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+            return app.UseMiddleware<CasSingleSignOutMiddleware>();
+        }
+
+        public static IApplicationBuilder UseCasSingleSignOut(this IApplicationBuilder app, ITicketStore store)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+            if (store == null) throw new ArgumentNullException(nameof(store));
+            return app.UseMiddleware<CasSingleSignOutMiddleware>(store);
+        }
+    }
+}
